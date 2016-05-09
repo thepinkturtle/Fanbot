f64a91981dba47d4ac2d76df0a8bb09297c93a4a
thepinkturtle <devinlax@hotmail.com>
2016-05-09 00:57:13 -0600

    Update test.py
    Added a bunch of comments and deleted the temp sensor code since it will no longer be used.

--- senior_project/openCV/images/test.py
+++ senior_project/openCV/images/test.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python
 import cv2
 import sys
-import Adafruit_TMP.TMP006 as TMP006
 import time
 import RPi.GPIO as gpio
 import thread
 import spidev
-
 
 #servo setup
 gpio.setmode(gpio.BOARD)
@@ -15,10 +13,6 @@ servo = gpio.PWM(11,50)
 servo.start(7.5)
 servo.ChangeDutyCycle(0)
 
-#temp sensor setup
-tempSensor = TMP006.TMP006()
-tempSensor.begin(samplerate=TMP006.CFG_8SAMPLE)
-
 #spi setup
 spi = spidev.SpiDev()
 spi.open(0,0)
@@ -26,8 +20,8 @@ spi.open(0,0)
 #arrays and such
 currentPos = 7.5
 CFace = 0
-moved_right = False
-moved_left = True
+max_right_pos = False
+max_left_pos = True
 minPos = 3
 maxPos = 11.5
 rangeRight = 253
@@ -35,8 +29,9 @@ rangeLeft = 138
 startTime = 0
 fClap = False
 sClap = False
+# If it's moving to fast and not stoping on a face mess with this variable
+incrementServo = .40 
 
-#previow ranges 135,255
 #webcam face detection
 cascPath = sys.argv[1]
 faceCascade = cv2.CascadeClassifier(cascPath)
@@ -45,8 +40,10 @@ video_capture = cv2.VideoCapture(0)
 video_capture.set(3, 320)
 video_capture.set(4, 240)
 
-#functions
+# Functions
 
+# This is for audio detection namely, the clap sensor. Gets the bits from the MCP 
+# and turns it into something we can use. 
 def getAdc (channel):
 
 	global startTime
@@ -78,7 +75,8 @@ def getAdc (channel):
 		time.sleep(.1)	
 		adcOut = 0
 		servo.ChangeDutyCycle(3)
-	
+
+# This is the clap detection. After two claps it should rescan the roon.
 def clap_detect():
 	
 	global fClap
@@ -95,50 +93,63 @@ def clap_detect():
 			print"time's up! at: {0} seconds".format(time.time() - startTime)
 			fClap = False
 
+# Checks if the servo is in the max position to the left or right. If its not then it just
+# moves the servo to the right until it's in the max right position and then moves it to the
+# the max left position.
 def scan():
 	global currentPos
-	global moved_right
-	global moved_left
-	if not moved_right: 
+	global max_right_pos
+	global max_left_pos
+	
+	if not max_right_pos: 
 		servo_right()
 		if currentPos >= maxPos:
-			moved_right = True
-			moved_left = False
-	if not moved_left:
+			max_right_pos = True
+			max_left_pos = False
+	if not max_left_pos:
 		servo_left()
 		if currentPos <= minPos:
-			moved_right = False
-			moved_left = True 
-	
+			max_right_pos = False
+			max_left_pos = True 
+
+# Moves the servo to the left once. But if its already at its max left position (minPos)
+# then it won't move left anymore
 def servo_left():
 	global currentPos
+	# Checks to see if its already at the max left (minPos) posistion
 	if currentPos > minPos:
-		currentPos = currentPos - .40
+		currentPos = currentPos - incrementServo
 		servo.ChangeDutyCycle(currentPos)
-	time.sleep(.02)
-	servo.ChangeDutyCycle(0)
+	time.sleep(.02) # Sleep because it reduces jitter
+	servo.ChangeDutyCycle(0) # Stop sending a signal servo also to stop jitter
 
-
+# Moves the servo to the left once. But if its already at its max right position (maxPos)
+# then it won't move right anymore
 def servo_right():
 	global currentPos
+	# Checks to see if its already at the max right (maxPos) posistion
 	if currentPos < maxPos:
-		currentPos = currentPos + .40
+		currentPos = currentPos + incrementServo
 		servo.ChangeDutyCycle(currentPos)
-	time.sleep(.02)
-	servo.ChangeDutyCycle(0)
+	time.sleep(.02) # Sleep because it reduces jitter
+	servo.ChangeDutyCycle(0) # Stop sending a signal servo also to stop jitter
 
-def track_face(new_cValue):
+# If the face is within the predetermined range don't do anything. If its outside of the range
+# Adjust the servo so that the face is back in the range again. This is misleading though
+# because the SERVO is turning left, however its left is our right and vice-verca.
+def track_face(face_position):
 
-	#turn left
-	if new_cValue > rangeRight:
+	# turn the SERVO to the left (our right)
+	if face_position > rangeRight:
 		servo_left()
 	
-	#turn right
-	if new_cValue < rangeLeft:
+	# turn the SERVO to the right (our left)
+	if face_position < rangeLeft:
 		servo_right()
 	time.sleep(.01)
 	servo.ChangeDutyCycle(0)
 
+# might get rid of this
 try: 
 	thread.start_new_thread(clap_detect, ())
 except:
@@ -146,35 +157,31 @@ except:
 
 
 while True:
-	
-#	time.sleep(.01)	
-	#capture frame by frame
-
+	# capture frame by frame
 	ret, frame = video_capture.read()
-#	grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+	
+	# find the position of the face
 	face = faceCascade.detectMultiScale(
 		frame,
 		scaleFactor = 1.3,
 		minNeighbors = 1,
 		minSize = (40,40),
 		flags = (cv2.CASCADE_DO_CANNY_PRUNING + cv2.CASCADE_FIND_BIGGEST_OBJECT + cv2.CASCADE_DO_ROUGH_SEARCH + cv2.CASCADE_SCALE_IMAGE))
-	#cv2.CASCADE_SCALE_IMAGE
-	# draw the rectangle around a face
+
+	# draw the rectangle around and face find the center of the face (CFace)
 	for (x, y, w, h) in face:
 		cv2.rectangle(frame, (x, y), (x+w,y+h), (0,0,255))
 		CFace = (w/2+x)
+		
 	# display the resulting frame
 	cv2.imshow('Video', frame)
 	
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
-	temp = tempSensor.readObjTempC()		
+	
 	#if we found a face send the position to the servo
-	if CFace != 0 and temp > 16 and temp < 28:
+	if CFace != 0:
 		track_face(CFace)
-#		print "temperature: {0}".format(temp)
-#		if temp > 19 and temp < 23:
 
 	else:
 		scan()
